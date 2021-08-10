1e26ab7108c43df68b19300044e274779269b4c1
haribowoo <85086591+hariBoWoo@users.noreply.github.com>
2021-08-10 15:02:07 +0900

    1st

---
+++ catalog-service/src/main/java/com/example/catalogservice/dto/ResponseCatalog.java
@@ -0,0 +1,14 @@
+package com.example.catalogservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseCatalog {
+    private String productId;
+    private String productName;
+    private Integer stock;
+    private Integer unitPrice;
+    private Data createdAt;
+}
